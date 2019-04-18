7dd3af660d429604df5bf25cacc56223f40d1016
ruhenheim <asd.uchiyama@gmail.com>
2019-04-18 13:51:52 +0900

    関心事外について問われた場合の出力文を微調整

--- thinkable.rb
+++ thinkable.rb
@@ -3,7 +3,7 @@ module Thinkable
     if respond_to? thing
       puts "私は#{self.method(thing).call}について考えています。"
     else
-      puts "私は何も考えていません。"
+      puts "私は#{thing}については、特に何も考えていません。"
     end
   end
 end
